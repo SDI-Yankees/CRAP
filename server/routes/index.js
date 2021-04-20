49359619b6acbbfd25f0b843fcd8d55c7e9da1b1
Eric Eash <ericeash@gmail.com>
2021-04-20 14:53:41 -0700

    Completed database and started on Express

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV]);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Hi world');
 });
+
+router.get('/:userid', (req, res)=>{
+  const userId = req.params.id;
+  knex.select('*')
+  .from('training_completions')
+  .where({user_id: userId})
+  .then(data => res.status(200).json(data))
+})
 
 module.exports = router;
