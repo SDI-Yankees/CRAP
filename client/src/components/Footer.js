a52209cb340d86308c440ddc7e77e1183dca0d43
Eric Eash <ericeash@gmail.com>
2021-04-22 21:51:33 -0700

    fixed footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,25 +5,30 @@ function Footer() {
   return (
     <div className="footer-container">
       <div className="left-footer">
-        <div style={{fontWeight:'bold'}}>Training Manager Contact Info <br/><br/>Help </div>
-        
-          <div className="card">
-            <div className="content">
-              <div className="front">
-                About the Dev Team
-              </div>
-              <div className="back">
-                Sam - likes CRAP
-                <br/>
-                Eric - does CRAP
-                <br/>
-                Jeremy - sees CRAP
-                <br/>
-                Derek - lives CRAP
-              </div>
+        <div style={{fontWeight:'bold'}}>
+          Training Manager Contact Info 
+          <br/>
+          <br/>
+          Help 
+        </div>
+      </div>
+      <div className="center-footer">
+        <div className="card">
+          <div className="content">
+            <div className="front">
+              About the Dev Team
+            </div>
+            <div className="back">
+              Sam - likes CRAP
+              <br/>
+              Eric - does CRAP
+              <br/>
+              Jeremy - sees CRAP
+              <br/>
+              Derek - lives CRAP
             </div>
           </div>
-        
+        </div>
       </div>
       <div className="right-footer">
         <div className="logo-container">
