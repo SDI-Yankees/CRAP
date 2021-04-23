fc0d94905020f3d20af329b28430d044137ca9a6
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-04-23 08:40:25 -0500

    fixed seed data credentials with bcrypt. no container for express since it requires a network re-work between the containers

--- server/seeds/seed_3credentials.js
+++ server/seeds/seed_3credentials.js
@@ -1,18 +1,21 @@
 const bcrypt = require('bcrypt');
 
-
+async function genHash(password)  {
+  const passwordHash =  await bcrypt.hash(password, 10)
+  return passwordHash
+} 
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('credentials').del()
-    .then(function () {
+    .then(async function () {
       // Inserts seed entries
       //username, password, email, user_id (foreign key)
       return knex('credentials').insert([
-        {username: 'snuffy', password: bcrypt.hash('password', 10), email: 'archibald.snuffy@us.af.mil', user_id: 1},
-        {username: 'supervisor', password: bcrypt.hash('password', 10), email: 'dave.chappelle@us.af.mil', user_id: 2},
-        {username: 'trainingmanager', password: bcrypt.hash('password', 10), email: 'timothy.lang@us.af.mil', user_id: 3},
-        {username: 'commander', password: bcrypt.hash('password', 10), email: 'eddie.uckerman@us.af.mil', user_id: 4},
+        {username: 'snuffy', password: await genHash('password'), email: 'archibald.snuffy@us.af.mil', user_id: 1},
+        {username: 'supervisor', password: await genHash('password'), email: 'dave.chappelle@us.af.mil', user_id: 2},
+        {username: 'trainingmanager', password: await genHash('password'), email: 'timothy.lang@us.af.mil', user_id: 3},
+        {username: 'commander', password: await genHash('password'), email: 'eddie.uckerman@us.af.mil', user_id: 4},
       ]);
     });
 };
