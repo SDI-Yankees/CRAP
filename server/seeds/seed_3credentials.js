85af55f14e2e083677c2dc16964a144340809d02
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-04-23 07:58:35 -0500

    first attempt at containerization for postgres and express

--- server/seeds/seed_3credentials.js
+++ server/seeds/seed_3credentials.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+
+
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -6,10 +9,10 @@ exports.seed = function(knex) {
       // Inserts seed entries
       //username, password, email, user_id (foreign key)
       return knex('credentials').insert([
-        {username: 'snuffy', password: 'password', email: 'archibald.snuffy@us.af.mil', user_id: 1},
-        {username: 'supervisor', password: 'password', email: 'dave.chappelle@us.af.mil', user_id: 2},
-        {username: 'trainingmanager', password: 'password', email: 'timothy.lang@us.af.mil', user_id: 3},
-        {username: 'commander', password: 'password', email: 'eddie.uckerman@us.af.mil', user_id: 4},
+        {username: 'snuffy', password: bcrypt.hash('password', 10), email: 'archibald.snuffy@us.af.mil', user_id: 1},
+        {username: 'supervisor', password: bcrypt.hash('password', 10), email: 'dave.chappelle@us.af.mil', user_id: 2},
+        {username: 'trainingmanager', password: bcrypt.hash('password', 10), email: 'timothy.lang@us.af.mil', user_id: 3},
+        {username: 'commander', password: bcrypt.hash('password', 10), email: 'eddie.uckerman@us.af.mil', user_id: 4},
       ]);
     });
 };
